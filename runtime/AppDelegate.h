3dc1a78ad3a2f44b8e13efb28ceaf7516e2da0d4
xiaozhenyangcode <xiaozheyangcode@163.com>
2017-05-10 09:33:55 +0800

    Initial Commit

---
+++ runtime/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  runtime
+//
+//  Created by 一天 on 2017/5/10.
+//  Copyright © 2017年 肖振阳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ runtime/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  runtime
+//
+//  Created by 一天 on 2017/5/10.
+//  Copyright © 2017年 肖振阳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
